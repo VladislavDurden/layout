43ba9deb44aacbf1c6c2da1c97bbd2f2a05d9853
VladislavDurden <petrovskiy.bsuir@gmail.com>
2018-10-07 04:35:30 +0300

    v1.0

---
+++ js/script.js
@@ -0,0 +1,69 @@
+window.onload = function () {
+    main = document.getElementById("main");
+
+}
+
+function showA() {
+    main.style.display = "block";
+    main.style.top = "200px";
+}
+
+function hideA() {
+    main.style.display = "none";
+}
+
+
+
+//МАСКА ДЛЯ ТЕЛЕФОНА
+
+jQuery(function ($) {
+    $("#phone").mask("+375(99)999-99-99");
+});
+
+jQuery(function ($) {
+    $("#phone2").mask("+375(99)999-99-99");
+});
+
+
+//СЛАЙДЕР
+
+$(function () {
+    $(".rslides").responsiveSlides({
+        auto: false,
+        speed: 800,
+        pager: true,
+        nav: true,
+        prevText: "<i class='fas fa-chevron-left fa-3x'></i>",
+        nextText: "<i class='fas fa-chevron-right fa-3x'></i>",
+        manualControls: ".custom",
+
+    });
+});
+
+
+
+//    КАРТЫ
+
+$.fn.lpMapInit = function () {
+
+    var lpMapOptions = {
+        center: [55.769018, 37.657188],
+        zoom: 16,
+        controls: ['fullscreenControl', 'zoomControl']
+    }
+
+    if (window.innerWidth < 600) {
+        lpMapOptions.behaviors = ['multiTouch'];
+    } else {
+        lpMapOptions.behaviors = ['drag'];
+    }
+
+    var lpMap = new ymaps.Map("lp-map", lpMapOptions);
+
+    lpPlacemark = new ymaps.Placemark(lpMapOptions.center, {
+        balloonContent: 'г. Москва, ул. Новая Басманная 12с2',
+        balloonContentHeader: 'офис 316'
+    });
+
+    lpMap.geoObjects.add(lpPlacemark);
+};
